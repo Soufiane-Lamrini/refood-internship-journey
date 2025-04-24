090e1a084cb086896ee6e47c3b49c86ec67ba254
Soufiane Lamrini <lmrinisoufiane77@gmail.com>
2025-04-24 13:53:52 +0100

    Ajouter des commantaires

--- javascript-cirriculm/01-variables/variables-exercices.js
+++ javascript-cirriculm/01-variables/variables-exercices.js
@@ -1,21 +1,28 @@
 // Task 1: Declare Variables
-var companyName = "Refood";
-let interName="John Doe";
-const internshipYear=2025;
+var companyName = "Refood";//Declarer un variable en utilse var
+let interName="John Doe";//Declarer un variable en utilse let
+const internshipYear=2025;//Declarer un variable en utilse const
 // Task 2: Print values
 
-console.log(companyName);
-console.log(interName);
-console.log(internshipYear);
+console.log(companyName);//Afficher la valeur du variable companyName
+console.log(interName);//Afficher la valeur du variable interName
+console.log(internshipYear);//Afficher la valeur du variable internshipYear
 
 // Task 3: Reassign Values
 companyName="Refood Tech";
 internName="Jane Smith";
-
+//internshipYear=2030 ===> il affiche un erreur
 // Task 4: Use different Data type
 
-let age=22;
-let isActive=true ;
-let skills=["JS", "HTML"];
-let details={department: "Tech", level: "Intern"};
+let age=22; // declarer un variable de type nombre
+let isActive=true ; // declarer un variable de type boolean
+let skills=["JS", "HTML"]; // declarer un variable de type array
+let details={department: "Tech", level: "Intern"}; // declarer un variable de type onject
 console.log(age, isActive, skills, details);
+
+//Task 5: typeof Check
+console.log(typeof companyName);//afficher un variable de type string
+console.log(typeof age);//afficher un variable de type nombre
+console.log(typeof isActive);//afficher un variable de type boolean
+console.log(typeof isActive);
+console.log(typeof details);
