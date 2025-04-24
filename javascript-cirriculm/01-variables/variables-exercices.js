1e2ada8343b858f9452f7939a00f50faca497ac1
Soufiane Lamrini <lmrinisoufiane77@gmail.com>
2025-04-24 13:38:36 +0100

    Exercices JS : variables (var, let, const + types)

---
+++ javascript-cirriculm/01-variables/variables-exercices.js
@@ -0,0 +1,21 @@
+// Task 1: Declare Variables
+var companyName = "Refood";
+let interName="John Doe";
+const internshipYear=2025;
+// Task 2: Print values
+
+console.log(companyName);
+console.log(interName);
+console.log(internshipYear);
+
+// Task 3: Reassign Values
+companyName="Refood Tech";
+internName="Jane Smith";
+
+// Task 4: Use different Data type
+
+let age=22;
+let isActive=true ;
+let skills=["JS", "HTML"];
+let details={department: "Tech", level: "Intern"};
+console.log(age, isActive, skills, details);
